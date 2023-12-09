14ff9879bc86f0273ab3109f831fd1f04b696b8b
langtonic <143374510+langtonic@users.noreply.github.com>
2023-12-09 19:09:09 +0800

    Part 2 Solution

---
+++ 2023/09/d9p2.py
@@ -0,0 +1,28 @@
+import sys
+
+histories = []
+
+for line in sys.stdin:
+	history = [int(x) for x in line.strip().split(' ')]
+	histories.append(history)
+
+def prevValue(sequence):
+	diffList = []
+	nextDiff = None
+	recur = False
+	for i in range(len(sequence) - 1):
+		diffList.append(sequence[i + 1] - sequence[i])
+	for x in diffList:
+		if not x == 0:
+			recur = True
+			break
+	if recur:
+		nextDiff = prevValue(diffList)
+	else:
+		nextDiff = 0
+	return (sequence[0] - nextDiff)
+
+sum = 0
+for history in histories:
+	sum += prevValue(history)
+print(sum)
